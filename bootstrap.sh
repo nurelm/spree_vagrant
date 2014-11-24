f2011c4de31fe9af25b192bfcb9e55b3e04c08ab
Sam Shaaban <sam@nurelm.com>
2014-11-23 21:39:08 -0500

    Update versions

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,13 +18,13 @@ su - vagrant -c "echo 'export PATH=\"~/.rbenv/bin:$PATH\"' >> ~/.bash_profile"
 su - vagrant -c "echo 'eval \"\$(rbenv init -)\"' >> ~/.bash_profile"
 su - vagrant -c "git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build"
 
-## Install Ruby 2.1.0
-su - vagrant -c "rbenv install 2.1.2"
-su - vagrant -c "rbenv global 2.1.2"
+## Install Ruby
+su - vagrant -c "rbenv install 2.1.5"
+su - vagrant -c "rbenv global 2.1.5"
 su - vagrant -c "rbenv rehash"
 
-## Install Rails 4.1.4
-su - vagrant -c "gem install rails -v 4.1.6"
+## Install Rails
+su - vagrant -c "gem install rails -v 4.1.8"
 su - vagrant -c "rbenv rehash"
 
 ## Install Spree gem
@@ -34,6 +34,6 @@ su - vagrant -c "rbenv rehash"
 ## Setup Spree app
 su - vagrant -c "rm -rf /vagrant/spree"
 su - vagrant -c "mkdir -p /vagrant/$SPREE_FOLDER"
-su - vagrant -c "rails _4.1.6_ new /vagrant/$SPREE_FOLDER -s"
+su - vagrant -c "rails _4.1.8_ new /vagrant/$SPREE_FOLDER -s"
 su - vagrant -c "spree install -A /vagrant/$SPREE_FOLDER"
 su - vagrant -c "cd /vagrant/$SPREE_FOLDER; bundle install"
