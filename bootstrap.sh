5c21dfc4a279d96ba3708344c51a535d1c80a941
Sarah Anderson <seanderson07@gmail.com>
2015-07-20 11:31:53 -0400

    Updates ruby version to 2.2.2 and Rails version to 4.2.2. Adds libraries needed for installing ruby 2.2.2.

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,9 +8,8 @@ sudo apt-get update
 sudo apt-get install -y git
 sudo apt-get install -y nodejs
 sudo apt-get install -y imagemagick
-sudo apt-get install -y libsqlite3-dev
-sudo apt-get install -y sqlite3
-sudo apt-get install -y build-essential
+sudo apt-get install -y libsqlite3-dev sqlite3
+sudo apt-get -y install autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev
 
 ## Install rbenv and rbenv-build to manage ruby environment
 su - vagrant -c "git clone https://github.com/sstephenson/rbenv.git ~/.rbenv"
@@ -19,12 +18,12 @@ su - vagrant -c "echo 'eval \"\$(rbenv init -)\"' >> ~/.bash_profile"
 su - vagrant -c "git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build"
 
 ## Install Ruby
-su - vagrant -c "rbenv install 2.1.5"
-su - vagrant -c "rbenv global 2.1.5"
+su - vagrant -c "rbenv install 2.2.2"
+su - vagrant -c "rbenv global 2.2.2"
 su - vagrant -c "rbenv rehash"
 
 ## Install Rails
-su - vagrant -c "gem install rails -v 4.1.8"
+su - vagrant -c "gem install rails -v 4.2.2"
 su - vagrant -c "rbenv rehash"
 
 ## Install Spree gem
@@ -34,6 +33,6 @@ su - vagrant -c "rbenv rehash"
 ## Setup Spree app
 su - vagrant -c "rm -rf /vagrant/spree"
 su - vagrant -c "mkdir -p /vagrant/$SPREE_FOLDER"
-su - vagrant -c "rails _4.1.8_ new /vagrant/$SPREE_FOLDER -s"
+su - vagrant -c "rails _4.2.2_ new /vagrant/$SPREE_FOLDER -s"
 su - vagrant -c "spree install -A /vagrant/$SPREE_FOLDER"
 su - vagrant -c "cd /vagrant/$SPREE_FOLDER; bundle install"
